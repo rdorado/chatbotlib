bfe44593fe218d13a8c55f80f0c13db67605a5b2
Ruben Dorado <ruben.dorados@gmail.com>
2018-02-27 15:19:59 -0500

    New version

--- chatbot/core.py
+++ chatbot/core.py
@@ -1,10 +1,10 @@
-import time
-import random
-import re
+import time, random, re, sys
+from lxml import etree
 
 class Chatbot:
 
-   def __init__(self, initialStateID, botPrompt, userPrompt):
+
+   def __init__(self, initialStateID=None, botPrompt="", userPrompt="", context={}):
       self.initialStateID=initialStateID
       self.currentState=None
       self.botPrompt=botPrompt
@@ -14,22 +14,56 @@ class Chatbot:
       self.states = {}
       self.messageQueue = []
       self.functions = []
+      self.isValid = None
+      self.externChatbots = []
+      self.context = context
+      globals()['context'] = self.context
+
+
+   def getBotPrompt(self):
+      return self.botPrompt
+
+
+   def getUserPrompt(self):
+      return self.userPrompt
 
    def isWaitingForInput(self):
       return self.waitingForInput 
 
-   def addFunction(self, string):
-      self.functions.append(string)
+
+   def addFunction(self, function):
+      self.functions.append(function)
+   
+   def addFunctionsFromCode(self, code):
+      ind1 = code.find("def")
+      while ind1 != -1: 
+         indn = code.find("(",ind1+3)
+         name = code[ind1+3:indn].strip() 
+         ind2 = code.find("def", ind1+3)
+         if ind2 == -1: 
+            self.addFunction(Function(code,name=name))
+         else: 
+            self.addFunction(Function(code,name=name))
+         ind1=ind2
+
+   def addExternChatbot(self, externChatbot):
+      self.externChatbots.append(externChatbot)
+      if externChatbot.getName() not in self.states:
+        self.states[externChatbot.getName()] = externChatbot
+
 
    def isFinished(self):
       return self.finished
 
+
    def getStateFromId(self, stateId):
       return self.states[stateId] 
+
 
    def setReady(self):
       self.currentState = self.getStateFromId(self.initialStateID)
       self.processState(self.currentState)
+
 
    def processState(self, state):
       startTP = state.getStart()
@@ -37,8 +71,10 @@ class Chatbot:
       self.messageQueue.extend(messages)
       self.waitingForInput = False
 
+
    def addState(self, state):
       self.states[state.getId()] = state
+
 
    def getNextMessage(self):
       if len(self.messageQueue) > 0:
@@ -48,34 +84,57 @@ class Chatbot:
          if delay > 0: time.sleep(delay)                  
          if len(self.messageQueue) == 0: self.waitingForInput = True
 
-         return message.text
+         return self.processMessage(message.text)
 
       self.waitingForInput = True
       return None 
       
+   def processMessage(self, text):
+      
+      if "${" in text:
+         indx1 = text.find("${")
+         resp = text[:indx1] 
+         while indx1 != -1: 
+            indx2 = text.find("}", indx1+2)
+            varname = text[indx1+2:indx2].strip()
+            resp += context[varname]
+            indx1 = text.find("${", indx2)
+            if indx1!=-1: resp+=text[indx2+1:indx1]
+            else: resp+=text[indx2+1:]
+         return resp
+      else:
+        return text
+
    def processInput(self, string):
       state = self.currentState
       
       for case in state.getCases():
-         if case.match(string, self):
+         nextStateId = case.match(string, self)
+         if nextStateId != None:
             messages = []	
             case.getOutput().processAsList(messages)
             self.messageQueue.extend(messages)
-            
-            nextStateId = case.getNextStateId()
 
             if nextStateId == None: 
                self.finished = True
                self.waitingForInput = False
             else:
-               self.currentState = self.getStateFromId(nextStateId)
-               messages = []
-               self.currentState.getStart().processAsList(messages)
-               self.messageQueue.extend(messages)
-               if self.currentState.isTerminalState(): self.finished = True 
-               else: self.finished = False
+               nextState = self.getStateFromId(nextStateId) 
+
+               if isinstance(nextState, State):
+                  self.currentState = nextState
+               
+                  messages = []
+                  self.currentState.getStart().processAsList(messages)
+                  self.messageQueue.extend(messages)
+                  if self.currentState.isTerminalState(): self.finished = True 
+                  else: self.finished = False
                       
-               self.waitingForInput = False    
+                  self.waitingForInput = False 
+
+               elif isinstance(nextState, ExternChatbot):  
+                  self.load(nextState.getSrc())
+ 
             return
 
       if state.hasLoopback():
@@ -86,6 +145,199 @@ class Chatbot:
          self.waitingForInput = False          
 
 
+   def clear(self):
+      pass
+
+   
+   ############################################################################################################
+   #
+   #  XML loading methods
+   ############################################################################################################
+   def load(self, filename):
+
+     try: 
+       parser = etree.XMLParser()
+       tree = etree.parse(filename, parser)
+       root = tree.getroot()
+       # TODO: validate with an Schema or a DTD
+
+       start=root.attrib["start"]
+       if "bot-prompt" in root.attrib:
+         botPrompt=root.attrib["bot-prompt"]
+       else:
+         botPrompt="chatbot> "
+       if "user-prompt" in root.attrib:
+         userPrompt=root.attrib["user-prompt"]
+       else:
+         userPrompt="you> "
+
+       self.clear()
+       self.__init__(start, botPrompt, userPrompt, context=self.context)
+
+       for el in root.getchildren():
+         if el.tag == "state":
+            self.readState(el)	
+         elif el.tag == "function":
+            self.readFunction(el)
+         elif el.tag == "code":
+            self.readCode(el)
+         elif el.tag == "extern-chatbot":
+            self.readExternalChatbot(el)
+
+       self.executeOnStart()
+       self.setReady()
+       return self
+    
+     except FileNotFoundError as err:
+       print("File not found: '"+filename+"'")
+       sys.exit(2)
+
+   def executeOnStart(self):
+      for funct in self.functions:
+         if funct.getName() == "onStart": 
+            exec(funct.getSource())
+            exec("onStart()")
+            return
+
+   def readExternalChatbot(self, root):
+      self.addExternChatbot(ExternChatbot(root.attrib['name'], root.attrib['src']))
+
+   def readCode(self, root):
+     try:
+       exec(root.text)
+     except SyntaxError as e:
+       print("Error loading the following code:\n\n"+root.text)
+       print(e)
+       sys.exit(2)
+     self.addFunctionsFromCode(root.text)
+
+
+   def readFunction(self, root):
+     try:
+       exec(root.text)
+     except SyntaxError as e:
+       print("Error loading the following function:\n\n"+root.text)
+       print(e)
+       sys.exit(2)
+     self.addFunction(Function(root.text))
+
+
+   def readState(self, root):
+      state = State(root.attrib["id"])
+   
+      for el in root.getchildren():
+
+         if el.tag == "start":
+            state.setStart( self.readStart(el) )           
+         elif el.tag == "input":
+            state.setInputProcessor( self.readInput(el) )           
+      self.addState(state)
+
+
+   def readStart(self, root):
+      resp = TextContainer()
+      for el in root.getchildren():
+         if el.tag == "sentence":
+           resp.addElement( self.readSentence(el) )           
+         elif el.tag in ["random","sequence"]:
+           self.readContainer(el, resp)          
+      return resp
+
+
+   def readInput(self,root):
+      resp = InputProcessor()
+      for el in root.getchildren():
+         if el.tag == "case": 
+            resp.addCase( self.readCase(el) )
+         elif el.tag == "loopback":
+            resp.setLoopback( self.readLoopback(el)) 
+      return resp
+
+
+   def readLoopback(self, root):
+      resp = Case()
+
+      outp = TextContainer()
+      self.readContainer(root, outp)
+      resp.setOutput(outp)
+
+      return resp
+
+
+   def readCase(self, root):
+      resp = Case()
+
+      outp = TextContainer()
+      self.readContainer(root, outp)
+      resp.setOutput(outp)
+   
+      try:
+         ctype = root.attrib["type"]
+      except KeyError:
+         print("Type attribute is mandatory for case elements, line "+str(root.sourceline))
+         sys.exit(2)
+
+      resp.setType(ctype)
+
+      if ctype == "pattern": 
+
+         try: 
+            resp.setPattern(root.attrib["pattern"])
+         except KeyError: 
+            print("A pattern should be specified for a case of type pattern, line "+str(root.sourceline))
+            sys.exit(2)
+
+      elif ctype == "function":
+
+         try: 
+            resp.setFunction(root.attrib["name"])
+         except KeyError: 
+            print("The name of a function should be specified for a pattern of type function, line "+str(root.sourceline))
+            sys.exit(2)
+
+      try:
+         nextState = root.attrib["next"] 
+         resp.setNextStateId(nextState)
+      except:
+         pass
+
+      return resp
+
+
+   def readSentence(self,root):
+      delay = 0
+
+      try:
+        delay = float(root.attrib["delay"])
+      except KeyError: pass
+
+      sent = Sentence(root.text, delay)
+      return sent
+
+
+   def readContainer(self, root, container=None):
+      if root.tag == "random":
+         cont = TextContainer("random")
+      else: 
+         cont = TextContainer("sequence")
+
+      if container!=None:
+         container.addElement(cont)
+
+      for el in root.getchildren():
+         if el.tag == "sentence":
+           cont.addElement( self.readSentence(el) )           
+         elif el.tag in ["random","sequence"]:
+           self.readContainer(el, cont)     
+        
+      return cont
+
+
+
+
+#################################################################
+# Class Message
+#################################################################
 class Message:
 
    def __init__(self, text, delay=0):
@@ -93,6 +345,56 @@ class Message:
       self.delay=delay
 
 
+
+#################################################################
+# Class ExternChatbot
+#################################################################
+class ExternChatbot:
+
+   def __init__(self, name, src):
+      self.name=name
+      self.src=src
+
+   def getName(self):
+      return self.name
+
+   def getSrc(self):
+      return self.src
+
+
+   def setName(self, name):
+      self.name=name
+
+   def setSrc(self, src):
+      self.src=src
+
+#################################################################
+# Class State
+#################################################################
+class Function:
+   
+   def __init__(self, src, name=None):
+      if name==None:
+         self.name = self.getFunctionNameFromSource(src)
+      else: 
+         self.name = name 
+      self.src = src
+ 
+   def getFunctionNameFromSource(self, src):
+      resp = ""
+      ind1 = src.find("def")
+      ind2 = src.find("(", ind1+3)
+      return src[ind1+3:ind2].strip()
+
+   def getName(self):
+      return self.name
+
+   def getSource(self):
+      return self.src
+
+#################################################################
+# Class State
+#################################################################
 class State:
 
    def __init__(self, id):
@@ -125,6 +427,10 @@ class State:
    def isTerminalState(self):
       return len(self.getCases())==0 and not self.hasLoopback()
 
+
+#################################################################
+# Class Sentence
+#################################################################
 class Sentence:
 
    def __init__(self, text, delay=0):
@@ -132,14 +438,18 @@ class Sentence:
       self.delay=delay
 
    
-      
+#################################################################
+# Class Code
+#################################################################   
 class Code:
 
    def __init__(self, code):
       self.code=code
 
 
-
+#################################################################
+# Class InputProcessor
+#################################################################   
 class InputProcessor:
    
    def __init__(self):
@@ -157,7 +467,9 @@ class InputProcessor:
       return self.loopback
 
 
-
+#################################################################
+# Class Case
+#################################################################   
 class Case:
 
    def __init__(self):
@@ -185,11 +497,14 @@ class Case:
 
    def match(self, string, bot):
       if self.type == "pattern":
-         return self.compiled.match(string) != None
+         if self.compiled.match(string) != None:
+            return self.nextStateId
+         else: return None
       elif self.type == "function":
          for funct in bot.functions:
-            exec(funct) 
-         exec("global resp\nresp = "+self.function+"(string)")
+            if funct.getName() == self.function: 
+              exec(funct.getSource())
+              exec("global resp\nresp = "+self.function+"(string)")
          return resp
 
    def setNextStateId(self, string):
@@ -199,6 +514,9 @@ class Case:
       return self.nextStateId
    
 
+#################################################################
+# Class TextContainer
+################################################################# 
 class TextContainer:
 
    def __init__(self, type=None):
@@ -234,7 +552,9 @@ class TextContainer:
          pass
   
 
-
+#################################################################
+# Class Output
+################################################################# 
 class Output:
    
    def __init__(self, type):
--- chatbot/loader.py
+++ chatbot/loader.py
@@ -1,14 +1,19 @@
 from lxml import etree
 import sys
-import chatbot.core
+from chatbot.core import Chatbot
 
 class ReadChatbotDefinitionException(Exception):
 
    def __init__(self, message):
       self.message = message
 
-def load(filename):
 
+def load(filename,context={}):
+  c = Chatbot(context=context)
+  c.load(filename)
+  return c
+
+'''
   try: 
     parser = etree.XMLParser()
     tree = etree.parse(filename, parser)
@@ -16,8 +21,14 @@ def load(filename):
     # TODO: validate with an Schema or a DTD
 
     start=root.attrib["start"]
-    botPrompt=root.attrib["bot-prompt"] 
-    userPrompt=root.attrib["user-prompt"]
+    if "bot-prompt" in root.attrib:
+      botPrompt=root.attrib["bot-prompt"]
+    else:
+      botPrompt="chatbot> "
+    if "user-prompt" in root.attrib:
+      userPrompt=root.attrib["user-prompt"]
+    else:
+      userPrompt="you> "
 
     bot = chatbot.core.Chatbot(start, botPrompt, userPrompt)
     
@@ -31,123 +42,8 @@ def load(filename):
     return bot
     
   except FileNotFoundError as err:
-    print("No such file or directory: '"+filename+"'")
+    print("File not found: '"+filename+"'")
     sys.exit(2)
-
-def readFunction(bot, root):
-  bot.addFunction(root.text)
-
-  try:
-    exec(root.text)
-  except SyntaxError as e:
-    print("Error loading the function '"+root.attrib['name']+"'")
-    print(e)
-    sys.exit(2)
-
-def readState(bot, root):
-   state = chatbot.core.State(root.attrib["id"])
-   
-   for el in root.getchildren():
-
-      if el.tag == "start":
-         state.setStart( readStart(el) )           
-      elif el.tag == "input":
-         state.setInputProcessor( readInput(el) )           
-   bot.addState(state)
-
-def readStart(root):
-   resp = chatbot.core.TextContainer()
-   for el in root.getchildren():
-      if el.tag == "sentence":
-        resp.addElement( readSentence(el) )           
-      elif el.tag in ["random","sequence"]:
-        readContainer(el, resp)          
-   return resp
-
-
-def readInput(root):
-   resp = chatbot.core.InputProcessor()
-   for el in root.getchildren():
-      if el.tag == "case": 
-         resp.addCase( readCase(el) )
-      elif el.tag == "loopback":
-         resp.setLoopback( readLoopback(el)) 
-   return resp
-
-def readLoopback(root):
-   resp = chatbot.core.Case()
-
-   outp = chatbot.core.TextContainer()
-   readContainer(root, outp)
-   resp.setOutput(outp)
-
-   return resp
-
-def readCase(root):
-   resp = chatbot.core.Case()
-
-   outp = chatbot.core.TextContainer()
-   readContainer(root, outp)
-   resp.setOutput(outp)
-   
-   try:
-      ctype = root.attrib["type"]
-   except KeyError:
-      print("Type attribute is mandatory for case elements, line "+str(root.sourceline))
-      sys.exit(2)
-
-   resp.setType(ctype)
-
-   if ctype == "pattern": 
-
-      try: 
-         resp.setPattern(root.attrib["pattern"])
-      except KeyError: 
-         print("A pattern should be specified for a case of type pattern, line "+str(root.sourceline))
-         sys.exit(2)
-
-   elif ctype == "function":
-
-      try: 
-         resp.setFunction(root.attrib["name"])
-      except KeyError: 
-         print("The name of a function should be specified for a pattern of type function, line "+str(root.sourceline))
-         sys.exit(2)
-
-   try:
-      nextState = root.attrib["next"] 
-      resp.setNextStateId(nextState)
-   except:
-      pass
-
-   return resp
-
-def readSentence(root):
-   delay = 0
-
-   try:
-     delay = float(root.attrib["delay"])
-   except KeyError: pass
-
-   sent = chatbot.core.Sentence(root.text, delay)
-   return sent
-
-
-def readContainer(root, container=None):
-   if root.tag == "random":
-      cont = chatbot.core.TextContainer("random")
-   else: 
-      cont = chatbot.core.TextContainer("sequence")
-
-   if container!=None:
-      container.addElement(cont)
-
-   for el in root.getchildren():
-      if el.tag == "sentence":
-        cont.addElement( readSentence(el) )           
-      elif el.tag in ["random","sequence"]:
-        readContainer(el, cont)     
-        
-   return cont
+'''
 
   
